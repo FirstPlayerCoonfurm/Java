323a2baf694c118c2561bb3b039eb465b4f2d3a7
FirstPlayerCoonfurm <119936322+FirstPlayerCoonfurm@users.noreply.github.com>
2024-02-04 19:30:17 +0700

    Overriding methods

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,31 @@
-import javax.swing.JOptionPane;
-
+class Alpha{
+    String name;
+    void show(){
+        System.out.println("Alpha:");
+        System.out.println("Name - "+name);
+    }
+    Alpha(String name){
+        this.name = name;
+    }
+}
+class Bravo extends Alpha{
+    int code;
+    @Override
+    void show() {
+        System.out.println("Bravo:");
+        System.out.println("Name - "+name);
+        System.out.println("Code - "+code);
+    }
+    Bravo(String name, int code){
+        super(name);
+        this.code = code;
+    }
+}
 public class Main {
-
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Твоя first программа");
+        Alpha objA = new Alpha("objA");
+        Bravo objB = new Bravo("objB", 123);
+        objA.show();
+        objB.show();
     }
 }
